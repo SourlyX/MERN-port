d653c17ca9d7fbf1177bde03f033ff5f52018e4e
Sourly <luisferua@gmail.com>
2026-08-24 10:46:36 -0600

    agrega componente placeholder para cambio de idioma

---
+++ client/src/components/LanguageSwitcher.jsx
@@ -0,0 +1,8 @@
+// This component is reserved for future development of the
+// English/Spanish language switcher.
+
+const LanguageSwitcher = () => {
+  return null;
+};
+
+export default LanguageSwitcher;
